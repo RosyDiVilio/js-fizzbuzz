c4f69874a0e5685f90ec1cbf703a7a2cd7d27b1f
RosyDiVilio <rosydivilio@gmail.com>
2024-05-15 15:18:35 +0200

    add: fizzbuzz not completed

---
+++ js/main.js
@@ -0,0 +1,18 @@
+'use strict'
+
+let three = 'Fizz';
+let five = 'Buzz';
+let threeFive = 'FizzBuzz';
+
+for (let i = 1; i <= 100; i++) {
+    console.log(i);
+
+    if (i % 3 === 0) {
+        console.log(three);
+    } else if (i % 5 === 0) {
+        console.log(five);
+    } else if ((i % 3 === 0) && (i % 5 === 0)) {
+        console.log(threeFive);
+    }
+}
+
